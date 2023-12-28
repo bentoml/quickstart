e3f31b3cb9850bfd35637134b970dbf75834b8a5
Ming Liang Dai <mingliang0921@gmail.com>
2023-12-28 20:06:56 +0000

    update code to be compatible with latest bentoml 1.2 sdk

--- import_model.py
+++ import_model.py
@@ -1,6 +1,5 @@
 import transformers
 import bentoml
-from bentoml.models import ModelContext
 
 model= "sshleifer/distilbart-cnn-12-6"
 task = "summarization"
@@ -8,9 +7,6 @@ pipeline = transformers.pipeline(task, model=model)
 
 with bentoml.models.create(
     name='summarization-model',
-    metadata={'model_name': model, 'task_name': task},
-    context=ModelContext(framework_name="", framework_versions={}),
-    signatures={},
 ) as model_ref:
     pipeline.save_pretrained(model_ref.path)
     print(f"Model saved: {model_ref}")
--- service.py
+++ service.py
@@ -1,20 +1,19 @@
 from __future__ import annotations
 import bentoml
-import bentoml_io
 from transformers import pipeline
 
-@bentoml_io.service(
+@bentoml.service(
     resources={"memory": "500MiB"},
-    traffic={"timeout": 1},
+    traffic={"timeout": 10},
 )
 class Summarization:
     model_ref = bentoml.models.get("summarization-model")
     
     def __init__(self) -> None:
         # Load model into pipeline
-        self.pipeline = pipeline(self.model_ref._info.metadata['task_name'], self.model_ref.path)
+        self.pipeline = pipeline('summarization', self.model_ref.path)
     
-    @bentoml_io.api
+    @bentoml.api
     def summarize(self, text: str) -> str:
         result = self.pipeline(text)
         return result[0]['summary_text']
