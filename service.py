c7cadad60565199bb38fb18b84378dffb725a40b
Sean Sheng <s3sheng@gmail.com>
2024-01-15 02:14:32 -0800

    core: update service to user hf hub directly

--- service.py
+++ service.py
@@ -2,18 +2,27 @@ from __future__ import annotations
 import bentoml
 from transformers import pipeline
 
+
+NEWS_PARAGRAPH = "Breaking News: In an astonishing turn of events, the small \
+town of Willow Creek has been taken by storm as local resident Jerry Thompson's cat, \
+Whiskers, performed what witnesses are calling a 'miraculous and gravity-defying leap.' \
+Eyewitnesses report that Whiskers, an otherwise unremarkable tabby cat, jumped \
+a record-breaking 20 feet into the air to catch a fly. The event, which took \
+place in Thompson's backyard, is now being investigated by scientists for potential \
+breaches in the laws of physics. Local authorities are considering a town festival \
+to celebrate what is being hailed as 'The Leap of the Century."
+
+
 @bentoml.service(
-    resources={"memory": "500MiB"},
+    resources={"cpu": "2"},
     traffic={"timeout": 10},
 )
 class Summarization:
-    model_ref = bentoml.models.get("summarization-model")
-    
     def __init__(self) -> None:
         # Load model into pipeline
-        self.pipeline = pipeline('summarization', self.model_ref.path)
+        self.pipeline = pipeline('summarization')
     
     @bentoml.api
-    def summarize(self, text: str) -> str:
+    def summarize(self, text: str = NEWS_PARAGRAPH) -> str:
         result = self.pipeline(text)
         return result[0]['summary_text']
